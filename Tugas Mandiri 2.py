468a2346f17d3628d1df8d87ac3d5cace75e4793
mdafa433 <58013917+mdafa433@users.noreply.github.com>
2022-10-02 18:49:55 +0700

    Add files via upload

---
+++ Tugas Mandiri 2.py
@@ -0,0 +1,15 @@
+beli = "y" or "Y"
+while beli == "y" or "Y":
+ print(" ========================================")
+ print(" Selamat Datang di Pasar Cibinong")
+ 
+
+
+ print("=========================================")
+ hrg=int(input("harga perkg=")) 
+ brt=int(input("berat pembelian="))
+ 
+ total=brt*hrg
+ print("Jumlah Bayar : ", total)
+ print("==============================================")
+ beli=input("Mau beli lagi? pilih Y jika Ya, pilih N jika Tidak (Y/N) = ")
